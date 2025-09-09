38366af8fdb1b9b9ec6cb42e3d86a172512ef8b2
Mateo <mperez571@unab.edu.co>
2025-09-08 22:11:37 -0500

    Add files via upload

---
+++ flor.py
@@ -0,0 +1,29 @@
+import turtle as tu
+
+import colorsys as cs
+
+tu.setup(1500,800)
+
+tu.speed(0)
+
+tu.tracer(10)
+
+tu.width(2)
+
+tu.bgcolor('black')
+
+for j in range(25):
+    
+    for i in range(15):
+        
+        tu.color(cs.hsv_to_rgb(i / 15))
+        tu.right(90)
+        tu.circle(200 - j * 4, 90)
+        tu.left(90)
+        tu.circle(200 - j * 4, 90)
+        tu.right(90)
+        tu.circle(50, 24)
+    
+tu.hideturtle()     
+
+tu.done()
